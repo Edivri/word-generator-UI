ef4860f4e378cb72208dec91eb63098dd5ca031b
Ben Edivri <eddyjamin@gmail.com>
2022-06-19 17:34:22 +0100

    Update app_widget.dart

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -7,6 +7,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'WhatWord',
       theme: ThemeData(
         primaryColor: AppColors.primary,
